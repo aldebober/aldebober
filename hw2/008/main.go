8de7e3a93a1a94f90bdecad35b8d8002418c4923
Yuriy <yuriy.pavlov@yolo.com>
2025-08-15 16:49:59 +0300

    finish templates

--- hw2/008/main.go
+++ hw2/008/main.go
@@ -1,46 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"os"
-	"strings"
+	"time"
 )
 
-type Person struct {
-	Name string
-	Age  int16
-}
-
 var tpl *template.Template
-
-var fm = template.FuncMap{
-	"uc": strings.ToUpper,
-	"fc": DoubleAge,
-}
 
 func init() {
 	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.func"))
 }
 
-func DoubleAge(a int16) int16 {
-	return a * 2
+var fm = template.FuncMap{
+	"FMDate": dayMonthYear,
+}
+
+func dayMonthYear(t time.Time) string {
+	return t.Format("01-02-2006")
 }
 
 func main() {
-	p1 := Person{"Lenin", 79}
-	p2 := Person{"Hitler", 63}
-
-	people := []Person{p1, p2}
-	data := struct {
-		P []Person
-	}{
-		people,
-	}
-
-	fmt.Println(data)
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.func", data)
+	t := time.Now()
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.func", t)
 	if err != nil {
 		log.Fatal(err)
 	}
