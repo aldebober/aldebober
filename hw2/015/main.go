164d1812a3fa949a1f3c1c278b74ea7e4175531b
Yuriy <yuriy.pavlov@yolo.com>
2026-02-03 16:22:02 +0200

    smth

---
+++ hw2/015/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		fmt.Fprintln(conn, "Hello here!")
+		conn.Close()
+	}
+}
